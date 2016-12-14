6c9c5040c47efcd84b76b91d0efb108ddb44ec11
karta1307845 <akarta1307845@gmail.com>
2016-12-14 11:17:29 +0800

    Chapter 5

--- RecursiveMethods.java
+++ RecursiveMethods.java
@@ -24,4 +24,50 @@ public class RecursiveMethods {
 			System.out.println(str.charAt(0));
 		}
 	}
+
+	public static int factorial(int n) {
+		if (n == 0) {
+			return 1;
+		} else {
+			return n * factorial(n - 1);
+		}
+	}
+
+	public static double power(double x, int n) {
+		if (n == 0) {
+			return 1;
+		} else {
+			return x * power(x, n - 1);
+		}
+	}
+
+	public static int gcd(int m, int n) {
+		if (m % n == 0) {
+			return n;
+		} else if (m < n) {
+			return gcd(n, m);
+		} else {
+			return gcd(n, m % n);
+		}
+	}
+
+	public static int fibonacci(int n) {
+		if (n <= 2) {
+			return 1;
+		} else {
+			return fibonacci(n - 1) + fibonacci(n - 2);
+		}
+	}
+
+	private static int fibo(int fibCurrent, int fibPrevious, int n) {
+		if (n == 1) {
+			return fibCurrent;
+		} else {
+			return fibo(fibCurrent + fibPrevious, fibCurrent, n - 1);
+		}
+	}
+
+	public static int fibonacciStart(int n) {
+		return fibo(1, 0, n);
+	}
 }
